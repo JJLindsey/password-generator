1bb1e610972424d9d704bdf6abcb90a4d34395a6
JJLindsey <jlindsey010@gmail.com>
2021-05-06 12:34:52 -0500

    evaluate input and revise prompts to confirms

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCharacters ="!@#$%^&*()";
+var specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*","(", ")"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numChar = [1,2,3,4,5,6,7,8,9];
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -14,19 +15,82 @@ function writePassword() {
 
 }
 //prompts for user input
-var passwordLength = alert("Please enter the number of characters for your password between 8 - 28.");
 
-var lowerCase = prompt("Do you want to include lower case letters?");
+function generatePassword() {
 
-var upperCase = prompt("Do you want to include upper case letters?");
+  var generatedPassword = "";
 
-var numbers = prompt("Do you enat to included numbers in your password?");
+  var passwordLength = prompt("Please enter the number of characters for your password between 8 - 28.");
 
-var specialCharacters = prompt("Do you want to include special characters?");
+  // use while() to validate length
+while (condition) {
+  
+}
+
+  var includesLowCase = prompt("Do you want to include lower case letters?");
+  
+  var includesUpCase = confirm("Do you want to include upper case letters?");
+  
+  var includesNum = confirm("Do you want to included numbers in your password?");
+  
+  var includeSpChar = confirm("Do you want to include special characters?");
+
+  //////////
+
+  var selectedPool = [];
+
+  // evaluate your input
+
+  if(includeSpChar) {
+    selectedPool = selectedPool.concat(specialCharacters)
+  }
+
+  if(includesNum) {
+    selectedPool = selectedPool.concat(numChar)
+  }
+
+  if (includesUpCase) {
+    selectedPool = selectedPool.concat(includesUpCase)
+  }
+
+  if (includesLowCase) {
+    selectedPool = selectedPool.concat(includesLowCase)
+  }
+
+
+  console.log(selectedPool)
+
+  // check which characters you would want to include on the character pool
+
+  // use for loop to generate a password by getting random characters from the generated pool
+
+
+  for (let index = 0; index < parseInt(passwordLength); index++) {
+    generatedPassword += selectedPool[Math.floor(Math.random()*selectedPool.length)]
+  }
+
+  return generatedPassword;
+}
+
 //validate user response
 
 
-//
+//random generate function
+
+
+// function myVoidFunction() {
+//   console.log("Hello")
+// }
+
+// function myReturnFunction() {
+//   return "Hello"
+// }
+
+// function anotherFunction() {
+//   var word = myReturnFunction();
+
+//   console.log(word)
+// }
 
 
 // Add event listener to generate button
